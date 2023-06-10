56cdf98953b263a1649d1ed8a297ee3a14f87299
martel42 <kovalevmaksim54@yandex.ru>
2023-06-10 04:14:02 +0300

    Changed gateway, bugfixes

--- gateway/src/main/kotlin/ru/mks/rsoi/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/ru/mks/rsoi/gateway/GatewayApplication.kt
@@ -1,15 +1,19 @@
 package ru.mks.rsoi.gateway
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignAutoConfiguration
+import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableAsync
 @EnableKafka
+@ImportAutoConfiguration
+@EnableFeignClients
 class GatewayApplication
 
 fun main(args: Array<String>) {
--- gateway/src/main/kotlin/ru/mks/rsoi/gateway/controller/GatewayController.kt
+++ gateway/src/main/kotlin/ru/mks/rsoi/gateway/controller/GatewayController.kt
@@ -4,17 +4,105 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import ru.mks.rsoi.gateway.response.PostResponse
+import ru.mks.rsoi.gateway.response.*
+import ru.mks.rsoi.gateway.service.*
+
 
 @RestController
 @CrossOrigin("http://localhost:8080/api/v1")
 @RequestMapping("api/v1")
-class GatewayController {
+class GatewayController(
+        val userClient: UserClient,
+        val userSubsClient: UserSubsClient,
+        val subsClient: SubsClient,
+        val roleClient: RoleClient,
+        val postClient: PostClient,
+        val blogClient: BlogClient
+) {
 
     @GetMapping("/dummy")
     @ResponseStatus(HttpStatus.OK)
     private fun dummyResponse() : String {
         return "ResponseEntity<String>(I'm gateway dummy!, HttpStatus.OK)"
+    }
+
+    @GetMapping("post/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getPostById(@PathVariable id: Long) : PostResponse = postClient.getPostById(id)
+    @GetMapping("post/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllPost() :List<PostResponse> = postClient.getAllPost()
+
+    @PostMapping("post/")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addPost(@RequestBody userResponse: PostResponse) {
+        postClient.addPost(userResponse)
+    }
+
+    @PutMapping("post/")
+    @ResponseStatus(HttpStatus.OK)
+    fun editPost(@RequestBody userResponse: PostResponse) {
+        postClient.editPost(userResponse)
+    }
+
+    @DeleteMapping("post/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deletePostById(@PathVariable id: Long) {
+        postClient.deletePostById(id)
+    }
+
+    @GetMapping("blog/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getBlogById(@PathVariable id: Long) : BlogResponse = blogClient.getBlogById(id)
+
+    @GetMapping("blog/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllBlog() :List<BlogResponse> = blogClient.getAllBlog()
+
+    @PostMapping("blog/")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addBlog(@RequestBody userResponse: BlogResponse) {
+        blogClient.addBlog(userResponse)
+    }
+
+    @GetMapping("role/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getRoleById(@PathVariable id: Long) : RoleResponse = roleClient.getRoleById(id)
+    @GetMapping("role/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllRole() :List<RoleResponse> = roleClient.getAllRole()
+
+    @GetMapping("subs/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getSubsById(@PathVariable id: Long) : SubsResponse = subsClient.getSubsById(id)
+    @GetMapping("subs/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllSubs() :List<SubsResponse> = subsClient.getAllSubs()
+
+    @GetMapping("userSubs/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getUserSubsById(@PathVariable id: Long) : UserSubsResponse = userSubsClient.getUserSubsById(id)
+    @GetMapping("userSubs/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllUserSubs() :List<UserSubsResponse> = userSubsClient.getAllUserSubs()
+
+    @PostMapping("userSubs/")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addUserSubs(@RequestBody userResponse: UserSubsResponse) {
+        userSubsClient.addUserSubs(userResponse)
+    }
+
+    @GetMapping("user/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getUserById(@PathVariable id: Long) : UserResponse = userClient.getUserById(id)
+    @GetMapping("user/all")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllUser() :List<UserResponse> = userClient.getAllUser()
+
+    @PostMapping("user/")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addUser(@RequestBody userResponse: UserResponse) {
+        userClient.addUser(userResponse)
     }
 
 
